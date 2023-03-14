266be443aac11b49ca6e5afa5b89b278ad0a395b
luis <luisrym@gmail.com>
2023-03-14 15:36:41 +0100

    add middleware body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.disable('x-powered-by');
 app.get('/', (req, res) => {
   res.json('Server is working!!');
