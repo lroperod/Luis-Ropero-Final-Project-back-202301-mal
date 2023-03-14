3f3b2b08708e83b0aa373c34393d929b8e4c2e4d
luis <luisrym@gmail.com>
2023-03-14 20:18:52 +0100

    create function to encrypt password and generate token

---
+++ src/api/auth/auth-utils.ts
@@ -0,0 +1,43 @@
+import crypto from 'node:crypto';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const createCypher = () => {
+  if (process.env.PASSWORD_ENCRYPTION_ALGORITHM === undefined) {
+    throw new Error('Encryption algorithm must be defined on env');
+  }
+
+  if (process.env.PASSWORD_ENCRYPTION_KEY === undefined) {
+    throw new Error('Encryption key must be defined on env');
+  }
+
+  const key = crypto
+    .createHash('sha256')
+    .update(String(process.env.PASSWORD_ENCRYPTION_KEY))
+    .digest('base64')
+    .substring(0, 32);
+
+  const iv = crypto.randomBytes(0);
+
+  return crypto.createCipheriv(
+    process.env.PASSWORD_ENCRYPTION_ALGORITHM,
+    key,
+    iv,
+  );
+};
+
+export const encryptPassword = (password: string) => {
+  const cipher = createCypher();
+  let encryptedPassword = cipher.update(password);
+  encryptedPassword = Buffer.concat([encryptedPassword, cipher.final()]);
+  return encryptedPassword.toString('hex');
+};
+
+export const generateJWTToken = (email: string) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET environment should be defined');
+  }
+
+  return jwt.sign({ email }, process.env.JWT_SECRET);
+};
