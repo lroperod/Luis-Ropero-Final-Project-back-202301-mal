7dec4299311c1877664696304b9eb5d1b4d1fee3
luis <luisrym@gmail.com>
2023-03-14 20:06:04 +0100

    connect auth-router with app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import authRouter from './api/auth/auth-router.js';
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.json());
+app.use('/auth', authRouter);
 
 export default app;
