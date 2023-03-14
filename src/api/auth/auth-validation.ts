39d1dea766f8f1d2896efb1febc99059d191e1e5
luis <luisrym@gmail.com>
2023-03-14 21:04:31 +0100

    create auth validation

---
+++ src/api/auth/auth-validation.ts
@@ -0,0 +1,11 @@
+import { Joi } from 'express-validation';
+
+export const authValidation = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required(),
+  }),
+};
+export default authValidation;
