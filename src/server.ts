b0abc5e6070b4e4236694ee63e44aeed3122d2e8
luis <luisrym@gmail.com>
2023-03-14 20:08:37 +0100

    create connection with db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import log from './logger.js';
+import connectDB from './database/connection.js';
 
 dotenv.config();
 
 const port = process.env.PORT ?? 3000;
+const mongoUrl = process.env.MONGO_URL ?? '';
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await connectDB(mongoUrl);
   log.info(`Server started in port ${port}`);
 });
