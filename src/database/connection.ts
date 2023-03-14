babadc47e5a4b3149b9343fe6216a83fd127209c
luis <luisrym@gmail.com>
2023-03-14 15:39:12 +0100

    create connection to db

---
+++ src/database/connection.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+import log from '../logger.js';
+
+const connectDB = async (urlBD: string) => {
+  try {
+    mongoose.set('strictQuery', false);
+    mongoose.set('debug', true);
+    mongoose.set('toJSON', {
+      virtuals: true,
+      transform(_doc, ret) {
+        delete ret.__v;
+        delete ret.id;
+      },
+    });
+
+    await mongoose.connect(urlBD);
+    log.info('Successfully connected to database');
+    return true;
+  } catch (error) {
+    log.error('Error connecting to database');
+    throw new Error(`Error: ${error}`);
+  }
+};
+
+export default connectDB;
