71ed5f564d83371fe12171f473e98cfb57993b4d
luis <luisrym@gmail.com>
2023-03-15 09:47:03 +0100

    delete try-catch in auth-controller

--- src/api/auth/auth-controller.ts
+++ src/api/auth/auth-controller.ts
@@ -1,5 +1,4 @@
 import { RequestHandler } from 'express';
-import logger from '../../logger.js';
 import { UserModel } from '../users/user-schema.js';
 import { AuthRequest, LoginResponse } from './auth-types.js';
 import { encryptPassword, generateJWTToken } from './auth-utils.js';
@@ -10,22 +9,18 @@ export const loginUserController: RequestHandler<
   AuthRequest
 > = async (req, res) => {
   const { email, password } = req.body;
-  try {
-    const filterUser = {
-      email,
-      password: encryptPassword(password),
-    };
 
-    const existingUser = await UserModel.findOne(filterUser).exec();
+  const filterUser = {
+    email,
+    password: encryptPassword(password),
+  };
 
-    if (existingUser === null) {
-      return res.sendStatus(404);
-    }
+  const existingUser = await UserModel.findOne(filterUser).exec();
 
-    const tokenJWT = generateJWTToken(email);
-    res.status(201).json({ accessToken: tokenJWT });
-  } catch (err) {
-    logger.error(err);
-    res.sendStatus(500);
+  if (existingUser === null) {
+    return res.sendStatus(404);
   }
+
+  const tokenJWT = generateJWTToken(email);
+  res.status(201).json({ accessToken: tokenJWT });
 };
