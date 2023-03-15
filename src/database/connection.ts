b0fa9d5fcfc7d8953501c14a7b1368fce3b98f07
luis <luisrym@gmail.com>
2023-03-15 10:17:05 +0100

    remove method that modify  json response from db

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -6,13 +6,6 @@ const connectDB = async (urlBD: string) => {
   try {
     mongoose.set('strictQuery', false);
     mongoose.set('debug', true);
-    mongoose.set('toJSON', {
-      virtuals: true,
-      transform(_doc, ret) {
-        delete ret.__v;
-        delete ret.id;
-      },
-    });
 
     await mongoose.connect(urlBD);
     log.info('Successfully connected to database');
