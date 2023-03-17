ddb0b497ded1189d760620a4266e16c3c5b1bfa3
luis <luisrym@gmail.com>
2023-03-17 17:12:10 +0100

    add middleware errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { errorHandler } from './utils/errors/error-handler.js';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -14,5 +15,6 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.use('/auth', authRouter);
+app.use(errorHandler);
 
 export default app;
