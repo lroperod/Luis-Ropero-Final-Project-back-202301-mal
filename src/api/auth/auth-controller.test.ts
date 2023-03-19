820a1102cb37d99274b932b4e4b3c1fe94c840ea
luis <luisrym@gmail.com>
2023-03-19 15:32:31 +0100

    create register controller and his test

--- src/api/auth/auth-controller.test.ts
+++ src/api/auth/auth-controller.test.ts
@@ -1,5 +1,5 @@
 import { UserModel } from './../users/user-schema';
-import { loginUserController } from './auth-controller';
+import { loginUserController, registerUserController } from './auth-controller';
 import { Request, Response } from 'express';
 import { encryptPassword, generateJWTToken } from './auth-utils';
 import { CustomHTTPError } from '../../utils/errors/custom-http-error';
@@ -44,6 +44,7 @@ describe('Given a loginUserController', () => {
     expect(response.status).toHaveBeenCalledWith(201);
     expect(response.json).toHaveBeenCalledWith(tokenJWT);
   });
+
   test('When the user does not exits, it shoud be responde with status 404 ', async () => {
     UserModel.findOne = jest.fn().mockImplementation(() => ({
       exec: jest.fn().mockResolvedValue(null),
@@ -51,6 +52,62 @@ describe('Given a loginUserController', () => {
     await loginUserController(request as Request, response as Response, next);
     expect(next).toHaveBeenCalledWith(
       new CustomHTTPError(404, 'User or password does not exists'),
+    );
+  });
+});
+
+describe('Given a registerUserController', () => {
+  const request = {
+    body: {
+      name: 'antonio',
+      email: 'antonio@gmail.com',
+      password: 'secreto123',
+    },
+  } as Partial<Request>;
+
+  const response = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    sendStatus: jest.fn(),
+  } as Partial<Response>;
+
+  const next = jest.fn();
+
+  const userRegister = {
+    ...request.body,
+    password: encryptPassword('secreto123'),
+  };
+
+  test('When the user does not exits, it shoud be responde with status 201', async () => {
+    UserModel.create = jest.fn();
+
+    await registerUserController(
+      request as Request,
+      response as Response,
+      next,
+    );
+    expect(UserModel.create).toBeCalledWith(userRegister);
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.json).toHaveBeenCalledWith({
+      msg: 'User has been successfully registered',
+    });
+  });
+
+  test('When the user exits, it should be respond with a with status 409', async () => {
+    UserModel.findOne = jest.fn().mockImplementation(() => ({
+      exec: jest.fn().mockResolvedValue(userRegister),
+    }));
+
+    await registerUserController(
+      request as Request,
+      response as Response,
+      next,
+    );
+    expect(next).toHaveBeenCalledWith(
+      new CustomHTTPError(
+        409,
+        'A user account already exits with this email address',
+      ),
     );
   });
 });
