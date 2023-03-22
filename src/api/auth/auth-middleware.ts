2979e15e0e51111021d0898f034d810aadfb223f
luis <luisrym@gmail.com>
2023-03-22 18:28:27 +0100

    create auth-middleware

---
+++ src/api/auth/auth-middleware.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomHTTPError } from '../../utils/errors/custom-http-error.js';
+
+export const authMiddleware: RequestHandler = (req, res, next) => {
+  const jwtToken = req.headers.authorization?.split(' ')[1];
+  if (!jwtToken) {
+    throw new CustomHTTPError(401, 'Unauthorized');
+  }
+
+  if (!process.env.JWT_SECRET) {
+    throw new CustomHTTPError(
+      500,
+      'JWT_SECRET environment variable is not defined',
+    );
+  }
+
+  const payload = jwt.verify(
+    jwtToken,
+    process.env.JWT_SECRET,
+  ) as jwt.JwtPayload;
+
+  res.locals.id = payload.id;
+  next();
+};
