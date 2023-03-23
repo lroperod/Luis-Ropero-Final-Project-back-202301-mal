3bcbd3b483c90cd6154835c9931ea9388e6c1add
luis <luisrym@gmail.com>
2023-03-23 04:55:24 +0100

    add getalltravels controller and test

--- src/api/travels/travel-controller.ts
+++ src/api/travels/travel-controller.ts
@@ -13,3 +13,17 @@ export const createTravelController: RequestHandler<
   await TravelModel.create(travel);
   resp.status(201).json({ msg: 'Your trip has been successfully created' });
 };
+
+const queryProjection = { __v: 0 };
+
+export const getAllTravelsController: RequestHandler<
+  unknown,
+  Travel[] | { msg: string }
+> = async (_req, res, next) => {
+  try {
+    const foundTravels = await TravelModel.find({}, queryProjection).exec();
+    res.json(foundTravels);
+  } catch (error) {
+    next(error);
+  }
+};
