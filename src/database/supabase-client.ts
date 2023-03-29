20c5ffed8985982b3a4d1f0ef05d66f69877bcdb
luis <luisrym@gmail.com>
2023-03-29 15:21:07 +0200

    refactor supabase client

--- src/database/supabase-client.ts
+++ src/database/supabase-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const bucketUrl = process.env.SUPABASE_BUCKET_URL ?? '';
-const buckectKey = process.env.SUPABASE_BUCKET_API_KEY ?? '';
+const bucketUrl = process.env.SUPABASE_BUCKET_URL as string;
+const buckectKey = process.env.SUPABASE_BUCKET_API_KEY as string;
 
 export const supabase = createClient(bucketUrl, buckectKey);
 export const TRAVEL_BUCKET_NAME = 'travelimg';
